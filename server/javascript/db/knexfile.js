65f9f57a37df00555b5b17d93cfd26348d63df89
Alex Reichert <reichertjalex@gmail.com>
2017-10-21 17:13:46 -0700

    Set up knexfile for prod with env variables

--- server/javascript/db/knexfile.js
+++ server/javascript/db/knexfile.js
@@ -2,9 +2,31 @@ module.exports = {
   dev: {
     client: 'postgresql',
     connection: {
-      database: 'demo',
-      user: '',
-      password: ''
+      port: process.env.BLOG_DB_PORT,
+      host: process.env.BLOG_DB_HOST,
+      database: process.env.BLOG_DB_NAME,
+      user: process.env.BLOG_DB_USER,
+      password: process.env.BLOG_DB_PW
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
+    }
+  },
+  prod: {
+    client: 'postgresql',
+    connection: {
+      port: process.env.BLOG_DB_PORT_PROD,
+      host: process.env.BLOG_DB_HOST_PROD,
+      database: process.env.BLOG_DB_NAME_PROD,
+      user: process.env.BLOG_DB_USER_PROD,
+      password: process.env.BLOG_DB_PW_PROD,
     },
     pool: {
       min: 2,
