02fa7afddf8ca42b7e1b50e48db7c3076afb0873
Alex Reichert <reichertjalex@gmail.com>
2018-02-17 22:17:20 -0800

    Initial pass at calculating potential earnings

--- client/react/app/components/reporting/ReportingStreaks.tsx
+++ client/react/app/components/reporting/ReportingStreaks.tsx
@@ -1,17 +1,59 @@
 import * as React from 'react';
-import { first, max, size } from 'lodash';
+import { first, max, size, groupBy, keys } from 'lodash';
 import { ReportingDatedItem } from '../../helpers/reporting';
-import { getStreakStats } from '../../helpers/utils';
+import { DatedItem, getStreakStats } from '../../helpers/utils';
 
 interface ReportingStreakProps {
   completedChecklists: ReportingDatedItem[];
   completedScorecards: ReportingDatedItem[];
 }
 
+// TODO: write unit tests for this
+const getTotalStreak = (
+  checklists: ReportingDatedItem[],
+  scorecards: ReportingDatedItem[]
+): number[] => {
+  const all = checklists.concat(scorecards);
+  const grouped = groupBy(all, 'date');
+  const items = keys(grouped)
+    .filter(date => grouped[date].length === 2)
+    .map(date => ({ date }));
+
+  return getStreakStats(items);
+};
+
+// TODO: unit tests
+const calculateEarnings = (streaks: number[]): number => {
+  const DAY = 10;
+  const WEEK = 30;
+  const MONTH = 200;
+  const THREE_MONTH = 400;
+  const SIX_MONTH = 800;
+  const YEAR = 1200;
+
+  return streaks.reduce((earnings, count) => {
+    const n = count % 365;
+    const days = n * DAY;
+    const weeks = Math.floor(n / 7) * WEEK;
+    const months = Math.floor(n / 30) * MONTH;
+    const threeMonthBonus = n > (3 * 30) ? THREE_MONTH : 0;
+    const sixMonthBonus = n > (6 * 30) ? SIX_MONTH : 0;
+    const yearBonus = n > 365 ? YEAR : 0;
+    const base = days + weeks + months;
+    const bonuses = threeMonthBonus + sixMonthBonus + yearBonus;
+
+    return earnings + base + bonuses;
+  }, 0);
+};
+
 const ReportingStreaks = ({ completedChecklists, completedScorecards }: ReportingStreakProps) => {
   const checklistStreak = getStreakStats(completedChecklists);
   const scorecardStreak = getStreakStats(completedScorecards);
+  const totalStreak = getTotalStreak(completedChecklists, completedScorecards);
+  const earnings = calculateEarnings(totalStreak);
   const style = { fontSize: 16 };
+
+  console.log('totals', totalStreak, earnings / 100);
 
   return (
     <div>
